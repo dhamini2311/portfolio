942de9c8e320d194b4deb8c6fac479df1f0f2376
dham2311 <dhamini2311@gmail.com>
2023-06-22 11:25:03 +0530

    first commit

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FrontendComponent } from './frontend/frontend.component';
+import { BackendComponent } from './backend/backend.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: AppComponent,
+  children: [
+    { path: 'header', component: HeaderComponent },
+    { path: 'home', component: HomeComponent},
+    { path: 'frontend',component:FrontendComponent},
+    { path: 'backend', component:BackendComponent},
+    { path:'contact', component:ContactComponent},
+    { path: '**', redirectTo: 'home' },]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FrontendComponent } from './frontend/frontend.component';
+import { BackendComponent } from './backend/backend.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FrontendComponent,
+    BackendComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
